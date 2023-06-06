ee9b80d6bbce4a11638f96d210f24c08b4f539c5
Nancy D. Arena <ndarena11@gmail.com>
2023-06-06 15:18:56 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(document.querySelector('#new-task-description').value);
+    form.reset();
+  })  
 });
+
+function buildToDo(task) {
+  const p = document.createElement('p');
+  const press = document.createElement('button');
+  press.addEventListener('click', handleDelete);
+  press.textContent = 'x';
+  p.textContent = `${task}`;
+  p.appendChild(press);
+  document.querySelector('#list').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
